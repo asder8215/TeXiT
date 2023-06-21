227f34c0636e9199fe261101b653ccb22abee6c6
asder8215 <asder8215@gmail.com>
2023-06-21 13:30:49 -0400

    No longer using deprecated None flags and will proceed with default flags

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv) {
     AdwApplication *app;
     int status;
 
-    app = adw_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    app = adw_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(main_window), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
