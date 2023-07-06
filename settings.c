de28aae4d44f26c928c9a2ad914091f7920728df
asder8215 <asder8215@gmail.com>
2023-07-06 13:46:21 -0400

    Reverted the click signal event from the blueprint file; can't figure out how to pass gpointer data to it

--- settings.c
+++ settings.c
@@ -1,7 +1,6 @@
 #include "settings.h"
 
 void settings_click(GtkButton* settings_btn, GtkWindow* window){
-    printf("BUTTON: %p; WINDOW: %p\n", settings_btn, window);
     GtkBuilder* settings_builder = gtk_builder_new_from_resource("/me/Asder8215/TeXiT/settings.ui");
     AdwPreferencesWindow* prefs_window = ADW_PREFERENCES_WINDOW(gtk_builder_get_object(settings_builder, "setting-dialog"));
     gtk_window_set_transient_for(GTK_WINDOW(prefs_window), window);
