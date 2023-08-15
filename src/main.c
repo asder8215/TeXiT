1a111afd29137e3f9ff4caecc918c7c0779df834
Megadash452 <marticraft680@gmail.com>
2023-08-15 15:10:27 -0400

    Can properly spawn multiple instances

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv) {
     AdwApplication *app;
     int status;
 
-    app = adw_application_new("me.Asder8215.TeXiT", G_APPLICATION_DEFAULT_FLAGS);
+    app = adw_application_new("me.Asder8215.TeXiT", G_APPLICATION_DEFAULT_FLAGS | G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(main_window), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
